f67448ecd3951915ba26aa31f186770a7a562656
aviral-1008 <aviralvishwakarma10@gmail.com>
2024-03-13 11:30:52 +0530

    myfile1

---
+++ 1_First.c
@@ -0,0 +1,11 @@
+/*
+Author : "Aviral"
+Date : "10:16:33"
+Month : "march"
+*/
+#include<stdio.h> // Including Header File
+
+
+void main(){
+    printf("hello World"); //Function to print the given string and came from stdio.h
+}
