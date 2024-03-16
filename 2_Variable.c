92d856dc45835023c013ee71e0efb393e60d1e54
aviral-1008 <aviralvishwakarma10@gmail.com>
2024-03-16 20:09:54 +0530

    updated

--- 2_Variable.c
+++ 2_Variable.c
@@ -6,5 +6,28 @@ time : 10:31 Pm
 #include<stdio.h>
 
 void main(){
+    // int a = -2144;
+    // printf("%d\n",a);
+    // printf("%u\n",a); //here it will print the correspondig value according  unsigned int chart 
+
+    // signed int b = -3276888;
+    // printf("%d\n",b);
+
+    // unsigned int  c = -3;
+    // printf("%u\n",c);
+
+    // float f = 12.56;
+    // printf("%f",f);
+
+    // long int g= 2545215645454554754;
+    // printf("%ld\n",g);
+ 
+    // short int h = 1311;
+    // printf("%d\n",h);
+
     
+
+    
+
 }
+// 4294967295
